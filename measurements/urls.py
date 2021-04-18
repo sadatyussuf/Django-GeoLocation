7c32391d4295071c52f802706983e22b52845803
sadatyussuf <tommyleespartan0@gmail.com>
2021-04-18 00:09:37 +0000

    set up the forms,urls,views,model and template

---
+++ measurements/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import calculate_distance
+
+app_name = 'measurements'
+urlpatterns = [
+    path('',calculate_distance,name='calculate-view')
+]
--- measurements/views.py
+++ measurements/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import  Measurement
+from .forms import MeasurementModelForm
 
 # Create your views here.
+def calculate_distance(request):
+    dis = get_object_or_404(Measurement,id=1)
+    form = MeasurementModelForm(request.POST or None)
+
+    context = {
+        'distance': dis,
+        'form':form
+        }
+
+    return render(request,'measurements/main.html',context)
+
