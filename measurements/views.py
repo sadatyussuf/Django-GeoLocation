7eef233aa7589e190b22ed1d66c0f726a48a0626
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2021-04-25 08:35:42 +0000

    complete basic logic for measurements/views.py

--- measurements/views.py
+++ measurements/views.py
@@ -7,6 +7,13 @@ def calculate_distance(request):
     dis = get_object_or_404(Measurement,id=1)
     form = MeasurementModelForm(request.POST or None)
 
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.destination = form.cleaned_data.get('destination')
+        instance.location = "Accra"
+        instance.distance = 5000.00
+        instance.save()
+
     context = {
         'distance': dis,
         'form':form
